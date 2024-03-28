68fd8165ede808ff749216158b0e94f4ae98687d
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-03-28 00:32:02 -0300

    criando estilo global e importando imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post.jsx";
+import { Header } from "./components/Header.jsx";
+import "./global.css";
 
 export function App() {
 	return (
 		<div>
+			<Header />
 			<Post
 				author="Lucas Magnago"
 				content="because we assume a modern browser is used and it supports all of the latest JavaScript and CSS features. This prevents syntax lowering, letting Vite serve modules as close as possible to the original source code."
