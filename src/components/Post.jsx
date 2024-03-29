ecb7087399792495e5b4776f2817edab0cbf1b68
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-03-29 16:35:56 -0300

    validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,7 +36,12 @@ export function Post({ author, content, publishedAt }) {
 	}
 
 	function handleNewCommentChange() {
+		event.target.setCustomValidity("");
 		setNewCommentText(event.target.value);
+	}
+
+	function handleNewCommentInvalid() {
+		event.target.setCustomValidity("Esse campo é obrigatório");
 	}
 
 	function deleteComment(commentToDelete) {
@@ -46,6 +51,8 @@ export function Post({ author, content, publishedAt }) {
 
 		setComments(commentsWithoutDeletedOne);
 	}
+
+	const isNewCommentEmpty = newCommentText == 0;
 
 	return (
 		<article className={styles.post}>
@@ -88,10 +95,14 @@ export function Post({ author, content, publishedAt }) {
 					placeholder="Deixe um comentário"
 					value={newCommentText}
 					onChange={handleNewCommentChange}
+					onInvalid={handleNewCommentInvalid}
+					required
 				/>
 
 				<footer>
-					<button type="submit">Comentar</button>
+					<button type="submit" disabled={isNewCommentEmpty}>
+						Comentar
+					</button>
 				</footer>
 			</form>
 
